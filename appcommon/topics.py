e3c9c06f6397f653a3530dbd07ab6aebd29ba93f
conradoplg <conradoplg@gmail.com>
2009-04-20 01:58:05 -0300

    Added topic specs

---
+++ appcommon/topics.py
@@ -0,0 +1,54 @@
+from pubsub.utils.topicspec import TopicTreeDefnSimple
+
+
+class CommonTopicTree(TopicTreeDefnSimple):
+    class commands:
+        'Events related to all commands'
+        
+        class created:
+            'The commands were created'
+            command_tree = 'The command tree'
+            _required = 'command_tree'
+        
+        class changed:
+            """The commands were changed.
+            
+            The change could be the name, description or shortcut
+            in any oh them"""
+            command_tree = 'The command tree'
+            commands = 'The command list'
+            accel_table = 'The new accelerator table'
+            _required = 'command_tree', 'commands', 'accel_table'
+            
+    class command:
+        'Events related to a single command'
+        
+        class execute:
+            'Execute the command specified by the ID'
+            ide = 'ID of the command'
+            _required = 'ide'
+            
+    class language:
+        'Events related to the program locale'
+        
+        class changed:
+            'The language was changed'
+        
+    class settings:
+        'Events related to the settings'
+        
+        class changed:
+            'Some or all of the settings were changed'
+            settings = 'The settings'
+            _required = 'settings'
+            
+    class setting:
+        'Events related to a specific setting'
+        
+        class changed:
+            'A setting was changed'
+            settings = 'The settings'
+            section = 'The section of the setting'
+            option = 'The option which was changed'
+            value = 'The new value'
+            _required = 'settings', 'section', 'option', 'value'
