9a382cb97e539ddc50d66cfbd439dae44da8577c
conradoplg <conradoplg@gmail.com>
2009-04-22 02:07:51 -0300

    Organized topics

--- libnavi/topics.py
+++ libnavi/topics.py
@@ -4,6 +4,8 @@ from appcommon import topics
 
 
 class TopicTree(TopicTreeDefnSimple):
+    # View topics
+    
     class program:
         'Events related to the program itself'
         
@@ -11,7 +13,17 @@ class TopicTree(TopicTreeDefnSimple):
             'The program was closed'
             pages = 'The note pages open'
             _required = 'pages'
+
+    class page:
+        'Events related to a specific page in the GUI'
             
+        class closing:
+            'A page is being closed by the user'
+            page = 'The page being closed'
+            _required = 'page'
+    
+    # Model topics
+          
     class note:
         'Events related to a specific note'
         
@@ -30,12 +42,4 @@ class TopicTree(TopicTreeDefnSimple):
             note = 'The closed note'
             _required = 'note'
             
-    class page:
-        'Events related to a specific page in the GUI'
-            
-        class closing:
-            'A page is being closed by the user'
-            page = 'The page being closed'
-            _required = 'page'
-
 pub.addTopicDefnProvider(TopicTree())
